1c1dba1d21d33e1d3a83c838201d8c291cb1a9be
BimalkaMuhandiram <bimalkamuhandiram9@gmail.com>
2024-11-21 18:16:40 +0530

    Save my local changes

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker main.py
-=======
-web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
->>>>>>> eae6ba9ca1849288d6d09601ded469cefc9cd181
+web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app
--- app.py
+++ app.py
@@ -4,24 +4,25 @@ import joblib
 from CarUser import CarUser
 
 app = FastAPI()
-joblib_in = open("car-recommender.joblib","rb")
-model=joblib.load(joblib_in)
 
-@app.get('/')
+# Load the machine learning model
+joblib_in = open("car-recommender.joblib", "rb")
+model = joblib.load(joblib_in)
+
+@app.get("/")
 def index():
-    return {'message': 'Cars Recommender ML API Text'}
+    return {"message": "Cars Recommender ML API"}
 
-@app.post('/car/predict')
-def predict_car_type(data:CarUser):
-    data = data.dict()
-    age=data['age']
-    gender=data['gender']
+@app.post("/car/predict")
+def predict_car_type(data: CarUser):
+    # Extract data from the request
+    age = data.age
+    gender = data.gender
 
+    # Perform prediction
     prediction = model.predict([[age, gender]])
     
-    return {
-        'prediction': prediction[0]
-    }
+    return {"prediction": prediction[0]}
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
